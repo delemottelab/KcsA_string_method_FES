1dc3a0c51d756c8ef0bd82912a62c7b60c37bbb2
sperezconesa <sperezconesa@gmail.com>
2022-02-18 15:50:56 +0100

    style(commitlint): change commitlint type from "prepsys" to "sysprep"

--- .husky/spc_commitlint.config.js
+++ .husky/spc_commitlint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'md', 'analysis', 'prepsys', 'bioinf'],] },
+  rules: { 'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'md', 'analysis', 'sysprep', 'bioinf'],] },
 }
